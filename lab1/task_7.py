09c77922fa3a71428f19216a9a287d756690dbdd
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-18 23:56:24 +0300

    Задание 7: формат времени

---
+++ lab1/task_7.py
@@ -0,0 +1,6 @@
+seconds = int(input("Секунд: "))
+minutes = seconds // 60
+new_second = seconds % 60
+
+print(str(minutes) +" мин" + " " + str(new_second) + " сек")
+# print(str(minutes) + ":" + str(new_second))

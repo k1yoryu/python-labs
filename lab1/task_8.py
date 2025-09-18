80fc7e0b526b6322740086e67b667a4861601d3f
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-19 00:03:44 +0300

    Задание 8: палиндром

---
+++ lab1/task_8.py
@@ -0,0 +1,9 @@
+text = input("Введите строку: ")
+if not text:
+    print("Пустая строка!")
+else:
+    textlower = text.lower()
+    if textlower == textlower[::-1]:
+        print("Это палиндром")
+    else:
+        print("Это не палиндром")

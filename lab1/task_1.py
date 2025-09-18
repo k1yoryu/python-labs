6601357c4d11e782ac8e2425bfe75a4620514765
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-18 23:08:44 +0300

    Задание 1: форматирование ФИО

---
+++ lab1/task_1.py
@@ -0,0 +1,9 @@
+surname = input("Введите фамилию: ")
+name = input("Введите имя: ")
+otchestvo = input("Введите отчество: ")
+
+if not surname or not name or not otchestvo:
+    print("Ошибка: все поля должны быть заполнены!")
+else:
+    result = surname + " " + name[0] + "." + otchestvo[0] + "."
+    print("Результат:", result)

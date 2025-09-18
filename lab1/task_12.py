589a2540f101efd00641b884e0d679be0489d2ca
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-19 01:57:39 +0300

    дописать перерасход, доп расчеты, налог и т.п

---
+++ lab1/task_12.py
@@ -0,0 +1,16 @@
+tarif_minutes = 60
+tarif_sms = 30
+tarif_price = 24.99
+gb_v_mb = 1024 #1gb=1024mb
+
+dopoln_minute_price = 0.89
+dopoln_minute_price = 0.59
+dopoln_mb_price = 0.79
+nalog = 0.02
+
+used_minutes = int(input("Введите количество израсходованных за месяц минут:"))
+used_sms = int(input("Введите количество отправленных за месяц SMS:"))
+used_internet = int(input("Введите объем использованного интернет-трафика (в МБ): "))
+
+
+

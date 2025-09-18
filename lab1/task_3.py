15eb16682b2f0d6c624548826c040fc1ab5c51ec
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-18 23:19:50 +0300

    Задание 3: проверка пароля

---
+++ lab1/task_3.py
@@ -0,0 +1,10 @@
+password = input("Введите пароль: ")
+if not password:
+    print("Пустая строка!")
+else:
+    if len(password) < 16:
+        print("Слишком короткий")
+    elif password.isalpha() or password.isdigit():
+        print ("Слабый пароль")
+    else:
+        print("Надежный пароль")

be4345e9192b9eaee0deaf775b9c296948117435
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-18 23:38:55 +0300

    Задание 5: магическое число

---
+++ lab1/task_5.py
@@ -0,0 +1,10 @@
+number = int(input("Введите число: "))
+if number % 7 == 0:
+    print("Магическое число!")
+else:
+    summa = 0
+    while number > 0:
+        cifra = number % 10
+        summa += cifra
+        number = number // 10
+    print(summa)

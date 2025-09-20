8fd91ead77decf89999190a786774430c8a25ca7
Aleksey Sak <alleksej.sak@gmail.com>
2025-09-20 21:04:00 +0300

    Очистка основной ветки master

--- lab1/task_08.py
+++
@@ -1,9 +0,0 @@
-text = input("Введите строку: ")
-if not text:
-    print("Пустая строка!")
-else:
-    textlower = text.lower()
-    if textlower == textlower[::-1]:
-        print("Это палиндром")
-    else:
-        print("Это не палиндром")
